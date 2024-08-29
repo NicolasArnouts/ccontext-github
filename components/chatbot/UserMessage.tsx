b356fd8eca2fd603eb13f95cd4e02cbdf91113b7
Nicolas Arnouts <nicolas.arnouts@student.hogent.be>
2024-08-29 23:12:39 +0200

    fix: overflow-auto on UserMessage

--- components/chatbot/UserMessage.tsx
+++ components/chatbot/UserMessage.tsx
@@ -11,7 +11,7 @@ interface UserMessageProps {
 const UserMessage: React.FC<UserMessageProps> = ({ content }) => {
   return (
     <div className="w-full flex justify-end mb-4 ">
-      <div className=" text-white rounded-lg py-2 px-4 max-w-full overflow-scroll	p-4 bg-blue-400  ">
+      <div className=" text-white rounded-lg py-2 px-4 max-w-full overflow-auto	p-4 bg-blue-400  ">
         <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
       </div>
     </div>
